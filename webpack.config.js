d77f3ad6138186990a84673dfef056d9af12a4fc
Jonathan Funk <jonfunk88@hotmail.com>
2016-10-30 20:53:43 -0700

    more quick fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,52 @@
 const join = require('path').join;
 const PATHS = {
-	src: join(__dirname, 'src'),
-	fonts: join(__dirname, 'fonts'),
-	build: join(__dirname, 'build')
+  src: join(__dirname, 'src'),
+  fonts: join(__dirname, 'fonts'),
+  build: join(__dirname, 'build')
 };
 
 
 module.exports = {
-   entry: "./src/index.js",
-   devServer: {
-     inline: true,
-     host: '0.0.0.0',
-     port: '3000',
-     watchOptions: {
-         aggregateTimeout: 300,
-         poll: true
-     }
-   },
-   output: {
-		path: PATHS.build,
-		publicPath: '/pong/',
-		filename: 'bundle.js'
+  entry: {
+		src: join(PATHS.src, 'index.js')
 	},
-   module: {
-       loaders: [
-           {
-             test: /\.css$/,
-             loader: "style!css"
-           },
-           {
-             test: /\.scss$/,
-             loaders: ['style', 'css', 'sass?sourceMap']
-           },
-           {
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader',
-             query: {
-               presets: ['es2015']
-             }
-           },
-           {
-           test: /\.(eot|svg|ttf|woff|woff2)$/,
-           loader: 'file?name=public/fonts/[name].[ext]'
-           }
-       ]
-   }
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: '3000',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
+    }
+  },
+  output: {
+    path: PATHS.build,
+    publicPath: '/pong/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass?sourceMap']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        include : PATHS.fonts,
+        loader: `file?name=/fonts/[name].[ext]`
+      }
+    ]
+  }
 };
